8c8c617da3ab1fff2af9dbd30c66fc4e62687322
Charles Sharp <25charlessharp@usd266.com>
2023-01-23 11:40:49 -0600

    first idea

--- ShortLongShort.java
+++ ShortLongShort.java
@@ -1,5 +1,11 @@
 public class ShortLongShort {
     public static void main(String[] args) {
-        
+        if(args[0].length() < args[1].length()){
+            System.out.println(args[0] + args[1] + args[0]);
+        }
+        else{
+            System.out.println(args[1] + args[0] + args[1]);
+        }
+
     }
 }
