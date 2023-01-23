b4395c258504967b70c430c56da09b0ed7ea5413
Charles Sharp <25charlessharp@usd266.com>
2023-01-23 11:51:30 -0600

    first guess

--- NegatePhrase.java
+++ NegatePhrase.java
@@ -1,5 +1,12 @@
 public class NegatePhrase {
     public static void main(String[] args) {
-        
+        String text = args[0].substring(0,2);
+        if(text == "not"){
+            System.out.println(args[0]);
+        }
+        else{
+            System.out.println("not " + args[0]);
+        }
+
     }
 }
