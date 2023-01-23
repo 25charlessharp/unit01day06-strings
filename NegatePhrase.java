a2cf16a82d3ff2518ff46ebbecfc3579ff674151
Charles Sharp <25charlessharp@usd266.com>
2023-01-23 11:53:21 -0600

    hopefully fixed

--- NegatePhrase.java
+++ NegatePhrase.java
@@ -1,7 +1,7 @@
 public class NegatePhrase {
     public static void main(String[] args) {
         String text = args[0].substring(0,2);
-        if(text == "not"){
+        if(text.equals("not")){
             System.out.println(args[0]);
         }
         else{
