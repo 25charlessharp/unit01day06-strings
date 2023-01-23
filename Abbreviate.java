21587f783c5081059f792c639fb9e7981d603886
Charles Sharp <25charlessharp@usd266.com>
2023-01-23 12:00:09 -0600

    first try

--- Abbreviate.java
+++ Abbreviate.java
@@ -1,5 +1,14 @@
 public class Abbreviate {
     public static void main(String[] args) {
-        
+        int text = args[0].length();
+        if(text >= 8){
+            String first = args[0].substring(0,2);
+            String second = args[0].substring(-2,0);
+
+            System.out.println(first + "..." + second );
+        }
+        else{
+            System.out.println(args[0]);
+        }
     }
 }
