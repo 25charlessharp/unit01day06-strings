c7051f0033cb75651deb88217b6b8c00e2f3336f
Charles Sharp <25charlessharp@usd266.com>
2023-02-01 11:50:41 -0600

    deleted TODO

--- Abbreviate.java
+++ Abbreviate.java
@@ -1,5 +1,4 @@
-// great, except again:
-// TODO: you have to actually make a new String
+
 public class Abbreviate {
     public static void main(String[] args) {
         String text = args[0];
--- ShortLongShort.java
+++ ShortLongShort.java
@@ -1,4 +1,3 @@
-// TODO: this assignment asked you to actually create a new String (i.e. define it in a variable), and not just print a result
 public class ShortLongShort {
     public static void main(String[] args) {
         String shorter = args[0];
