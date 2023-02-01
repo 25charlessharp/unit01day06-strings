e5a7958304691b06912d41845ee1c2b67b9d4ab9
Charles Sharp <25charlessharp@usd266.com>
2023-02-01 11:42:34 -0600

    created strings

--- NegatePhrase.java
+++ NegatePhrase.java
@@ -1,13 +1,14 @@
-// good, except:
-// TODO: this assignment asked you to actually create a new String (i.e. define it in a variable), and not just print a result
+
 public class NegatePhrase {
     public static void main(String[] args) {
-        String text = args[0].substring(0,3);
-        if(text.equals("not")){
-            System.out.println(args[0]);
+        String text = args[0];
+        String not = args[0].substring(0,3);
+        if(not.equals("not")){
+            System.out.println(text);
         }
         else{
-            System.out.println("not " + args[0]);
+            String newText = "not "+ text;
+            System.out.println(newText);
         }
 
     }
