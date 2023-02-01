4e767fa0b444a4053e6ac8cfc6d3b315be5658ea
Charles Sharp <25charlessharp@usd266.com>
2023-02-01 11:37:43 -0600

    created strings

--- ShortLongShort.java
+++ ShortLongShort.java
@@ -1,11 +1,20 @@
 // TODO: this assignment asked you to actually create a new String (i.e. define it in a variable), and not just print a result
 public class ShortLongShort {
     public static void main(String[] args) {
-        if(args[0].length() < args[1].length()){
-            System.out.println(args[0] + args[1] + args[0]);
+        String shorter = args[0];
+        String longer = args[1];
+
+
+
+
+
+        if(shorter.length() < longer.length()){
+            String sls = shorter + longer + shorter;
+            System.out.println(sls);
         }
         else{
-            System.out.println(args[1] + args[0] + args[1]);
+            String sls = longer + shorter + longer;
+            System.out.println(sls);
         }
 
     }
