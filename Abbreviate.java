f19cc3bcd5bad159776ba18f4b9f38c9f88664a5
Charles Sharp <25charlessharp@usd266.com>
2023-02-01 11:47:26 -0600

    possibly done

--- Abbreviate.java
+++ Abbreviate.java
@@ -2,15 +2,17 @@
 // TODO: you have to actually make a new String
 public class Abbreviate {
     public static void main(String[] args) {
-        int text = args[0].length();
-        if(text >= 8){
+        String text = args[0];
+        int textLength = args[0].length();
+        if(textLength >= 8){
             String first = args[0].substring(0,2);
             String second = args[0].substring(text-2,text);
+            String newText = first + "..." + second;
 
-            System.out.println(first + "..." + second );
+            System.out.println(newText);
         }
         else{
-            System.out.println(args[0]);
+            System.out.println(text);
         }
     }
 }
